ef4bcba102d815dabf86d0d14a460db1be03512d
DoctorFear <thaisonx2602@gmail.com>
2026-03-14 23:29:40 +0700

    Done Tuần 1 2 | TS MP

---
+++ pytest.py
@@ -0,0 +1,5 @@
+import torch
+
+print("Torch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+print("Torch built with CUDA:", torch.cuda.get_device_name(0))
--- src/embedder.py
+++ src/embedder.py
@@ -3,7 +3,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 embedder = HuggingFaceEmbeddings(
     model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
-    model_kwargs={'device': 'cpu'},          # 'cuda' nếu có GPU
+    model_kwargs={'device': 'gpu'},          # 'cuda' nếu có GPU
     encode_kwargs={'normalize_embeddings': True}
 )
 
