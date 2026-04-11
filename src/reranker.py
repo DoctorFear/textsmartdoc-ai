c1ba4856f7edff43e4ef73d7fb98f4f26c19c754
Shovul <140464903+Shovul@users.noreply.github.com>
2026-04-11 07:20:15 +0700

    Conversational & Reranking implmentation

---
+++ src/reranker.py
@@ -0,0 +1,17 @@
+from sentence_transformers import CrossEncoder
+
+# good balance model
+model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+def rerank(query, docs, top_k=3):
+    pairs = [(query, doc.page_content) for doc in docs]
+
+    scores = model.predict(pairs)
+
+    ranked = sorted(
+        zip(docs, scores),
+        key=lambda x: x[1],
+        reverse=True
+    )
+
+    return [doc for doc, _ in ranked[:top_k]]
